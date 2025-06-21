32a83904b3a3b10e42fe73f62eff75be69667afb
Vasile Crăciunescu <vasile.craciunescu@gmail.com>
2025-06-21 18:56:31 +0300

    setat tema light ca fiind default

--- src/store/useConfig.ts
+++ src/store/useConfig.ts
@@ -3,7 +3,7 @@ import { persist } from "zustand/middleware";
 import useGraph from "../features/editor/views/GraphView/stores/useGraph";
 
 const initialStates = {
-  darkmodeEnabled: true,
+  darkmodeEnabled: false,
   collapseButtonVisible: true,
   childrenCountVisible: true,
   imagePreviewEnabled: true,
